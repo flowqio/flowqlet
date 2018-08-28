7dfa4f9bc654fb24df2fca0fa9233100c6eb9645
stevensu <suwei007@gmail.com>
2018-08-28 22:40:53 +0800

    add flowqlet.go

---
+++ flowqlet.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	//Init router handler prefix and utils log configuration
+	_ "github.com/flowqio/flowqlet/router"
+
+	"github.com/flowqio/flowqlet/service"
+)
+
+func main() {
+
+	port := flag.Int("p", 8800, "listen port")
+	host := flag.String("h", "localhost", "listen address")
+	nodeID := flag.String("n", "env1", "nodeID")
+	token := flag.String("token", "", "join token")
+
+	flag.Parse()
+
+	if *token == "" {
+		fmt.Println("join token required.")
+		return
+	}
+
+	go service.OnBoard(*token, *nodeID, fmt.Sprintf("%s:%d", *host, *port))
+
+	srv := &http.Server{
+		Addr: *host + ":" + strconv.Itoa(*port),
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Infof("flowqlet [%s] listen at %s:%d ", *nodeID, *host, *port)
+	log.Fatal(srv.ListenAndServe())
+
+}
